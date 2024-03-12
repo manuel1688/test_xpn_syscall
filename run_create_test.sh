3090d64817fa3219584632ed9704169d779fd457
Manuel Otero <manuelotero@Manuels-MacBook-Pro.local>
2024-03-12 01:14:08 +0100

    test case para create

---
+++ run_create_test.sh
@@ -0,0 +1,2 @@
+
+env LD_PRELOAD=../../../src/xpn_syscall_intercept/xpn_syscall_intercept_create.so:../../../../mxml/libmxml.so.1:$LD_PRELOAD  XPN_CONF=./xpn.conf prueba_create.c /tmp/expand/P1/demo.txt  8
