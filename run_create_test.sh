bc0ad348448372b62d0fb19b2c31e009a9562ef2
Manuel Otero <manuelotero@Manuels-MacBook-Pro.local>
2024-03-12 12:35:45 +0100

    se agrega el xpn.conf

--- run_create_test.sh
+++ run_create_test.sh
@@ -1,2 +1,4 @@
 
-env LD_PRELOAD=../../../src/xpn_syscall_intercept/xpn_syscall_intercept_create.so:../../../../mxml/libmxml.so.1:$LD_PRELOAD  XPN_CONF=./xpn.conf prueba_create.c /tmp/expand/P1/demo.txt  8
+env LD_PRELOAD=../../../src/xpn_syscall_intercept/xpn_syscall_intercept_create.so:../../../../mxml/libmxml.so.1:$LD_PRELOAD  XPN_CONF=./xpn.conf ./prueba_create /tmp/expand/P1/demo.txt  8
+
+LD_PRELOAD=../../../src/xpn_syscall_intercept/xpn_syscall_intercept_create.so:$LD_PRELOAD  XPN_CONF=./xpn.conf ./prueba_create /tmp/expand/P1/demo.txt  8
