e9b6dd8ced939bc819107328e246b3836c693930
Manuel Otero <manuelotero@Manuels-MacBook-Pro.local>
2024-03-22 12:47:56 +0100

    prueba_create.c

--- prueba_create.c
+++ prueba_create.c
@@ -21,6 +21,9 @@ double get_time(void)
 int main(int argc, char *argv[]) 
 {
     printf("argc: %d\n", argc);
+    printf("argv[0]: %s\n", argv[0]);
+    printf("argv[1]: %s\n", argv[1]);
+    printf("argv[2]: %s\n", argv[2]);
     if (argc < 3)
 	{
         printf("\n");
