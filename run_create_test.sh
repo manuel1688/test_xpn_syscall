9bf2253a0bde7b9672cdc250c99e8a7cabb1334d
Manuel Otero <manuelotero@Manuels-MacBook-Pro.local>
2024-04-02 10:54:25 +0200

    ajuste en script de ejecucion

--- run_create_test.sh
+++ run_create_test.sh
@@ -1,1 +1,1 @@
-LD_PRELOAD="../../../src/bypass/xpn_bypass.so:../../../../mxml/libmxml.so.1:$LD_PRELOAD"  XPN_CONF="./xpn.conf" ./prueba_create /tmp/expand/P1/demo.txt  1
+LD_PRELOAD="../../../src/xpn_syscall_intercept/xpn_syscall_intercept_create.so:$LD_PRELOAD"  XPN_CONF="./xpn.conf" ./prueba_create /tmp/expand/P1/demo.txt 3
