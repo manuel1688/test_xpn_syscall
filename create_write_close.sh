c4bb080f207b60cbd1e60a00a0e4b92fdf21a4a1
Manuel Otero <manuelotero@Manuels-MacBook-Pro.local>
2024-04-04 09:39:00 +0200

    cambios en sh

--- create_write_close.sh
+++ create_write_close.sh
@@ -1,1 +1,1 @@
-LD_PRELOAD="../../../src/xpn_syscall_intercept/xpn_syscall_intercept_w.so:$LD_PRELOAD"  XPN_CONF="./xpn.conf" ./prueba_create_write /tmp/expand/P1/demo.txt 3
+LD_PRELOAD="../../../src/xpn_syscall_intercept/xpn_syscall_intercept_w.so:$LD_PRELOAD"  XPN_CONF="./xpn.conf" ./create_write_close /tmp/expand/P1/demo.txt 3
