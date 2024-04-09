7e3e1a6ccdfca32b7cb3c9b06117e9146c2c031c
Manuel Otero <manuelotero@Manuels-MacBook-Pro.local>
2024-04-09 11:10:42 +0200

    ajuste

--- open_read_close.sh
+++ open_read_close.sh
@@ -1,1 +1,2 @@
-LD_PRELOAD="../../../src/xpn_syscall_intercept/xpn_syscall_intercept_w.so:$LD_PRELOAD"  XPN_CONF="./xpn.conf" ./open_read_close /tmp/expand/P1/demo.txt 3
+LD_PRELOAD=../../../src/xpn_syscall_intercept/xpn_syscall_intercept_w.so  XPN_CONF=./xpn.conf ./open_read_close /tmp/expand/P1/demo.txt 3
+
