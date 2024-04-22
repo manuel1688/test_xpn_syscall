839b8df1f301bda5bb6536e434bc0ee17b97f630
Manuel Otero <manuelotero@Manuels-MacBook-Pro.local>
2024-04-22 10:47:26 +0200

    se agrega el open_lseek64_close.c

--- Makefile
+++ Makefile
@@ -8,7 +8,7 @@
  MYFLAGS      = -O2 -Wall -D_REENTRANT -DPOSIX_THREADS -DHAVE_CONFIG_H
 
 # Rules
-all: prueba_create create_write_close open_read_close openat_read_close fopen_fread_fclose create_fwrite_fclose open64_read_close create_pwrite_close open_lseek_close
+all: prueba_create create_write_close open_read_close openat_read_close fopen_fread_fclose create_fwrite_fclose open64_read_close create_pwrite_close open_lseek_close open_lseek64_close
 
 prueba_create: prueba_create.o
 	$(CC) -o prueba_create prueba_create.o $(MYLIBPATH) $(LIBRARIES)
@@ -37,9 +37,12 @@ create_pwrite_close: create_pwrite_close.o
 open_lseek_close: open_lseek_close.o
 	$(CC) -o open_lseek_close open_lseek_close.o $(MYLIBPATH) $(LIBRARIES)
 
+open_lseek64_close: open_lseek64_close.o
+	$(CC) -o open_lseek64_close open_lseek64_close.o $(MYLIBPATH) $(LIBRARIES)
+
 %.o: %.c
 	$(CC) $(CFLAGS) $(MYFLAGS) $(MYHEADER) -c $< -o $@
 
 clean:
 	rm -f ./*.o
-	rm -f ./prueba_create ./create_write_close ./open_read_close ./openat_read_close ./fopen_fread_fclose ./create_fwrite_fclose ./open64_read_close ./create_pwrite_close ./open_lseek_close
+	rm -f ./prueba_create ./create_write_close ./open_read_close ./openat_read_close ./fopen_fread_fclose ./create_fwrite_fclose ./open64_read_close ./create_pwrite_close ./open_lseek_close ./open_lseek64_close
--- open_lseek64_close.c
+++ open_lseek64_close.c
@@ -24,16 +24,16 @@ int main ( int argc, char *argv[] )
 
 	if (argc < 3)
 	{
-		printf("\n") ;
-		printf(" Usage: %s <full path> <megabytes to read>\n", argv[0]) ;
-		printf("\n") ;
-		printf(" Example:") ;
+		printf("\n");
+		printf(" Usage: %s <full path> <megabytes to read>\n", argv[0]);
+		printf("\n");
+		printf(" Example:");
 		printf(" %s /tmp/expand/test_1 2\n", argv[0]);
-		printf("\n") ;
-		return -1 ;
+		printf("\n");
+		return -1;
 	}	
 
-	memset(buffer, 'a', BUFF_SIZE) ;
+	memset(buffer, 'a', BUFF_SIZE);
 	t_bc = get_time();
 	
 	fd1 = open(argv[1], O_RDWR);
@@ -41,18 +41,18 @@ int main ( int argc, char *argv[] )
 		printf("Error opening file\n");
 		return -1 ;
 	}
-	printf("%d = open('%s', %o)\n", fd1, argv[1], 00777) ;
+	printf("%d = open('%s', %o)\n", fd1, argv[1], 00777);
 
 	t_bw = get_time(); 
 
-	off_t offset = lseek(fd1, 0, SEEK_SET);
+	off_t offset = lseek64(fd1, 0, SEEK_SET);
 	if (offset == -1) {
 		printf("Error seeking file\n");
 		return -1;
 	}
-	printf("%ld = lseek(%d, 0, %d)\n", offset, fd1, SEEK_SET);
+	printf("%ld = lseek64(%d, 0, %d)\n", offset, fd1, SEEK_SET);
 
-	long mb = atoi(argv[2]) ;
+	long mb = atoi(argv[2]);
 	for (int i = 0; i < mb; i++)
 	{
 		ret = read(fd1, buffer, BUFF_SIZE);
@@ -62,11 +62,11 @@ int main ( int argc, char *argv[] )
 	t_aw = get_time() - t_bw;
 
 	ret = close(fd1);
-	printf("%d = close(%d)\n", ret, fd1) ;
+	printf("%d = close(%d)\n", ret, fd1);
 
 	t_ac = get_time() - t_bc;
 
-	printf("Bytes (KiB); Total time (ms); Read time (ms)\n") ;
-	printf("%f;%f;%f\n", ((double)mb * (double)BUFF_SIZE) / ((double)KB), t_ac * 1000, t_aw * 1000) ;
+	printf("Bytes (KiB); Total time (ms); Read time (ms)\n");
+	printf("%f;%f;%f\n", ((double)mb * (double)BUFF_SIZE) / ((double)KB), t_ac * 1000, t_aw * 1000);
 	return 0;
 }
