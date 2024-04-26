d5cf0067889c56db71e2c099ce2e5e2706a0d823
Manuel Otero <manuelotero@Manuels-MacBook-Pro.local>
2024-04-26 10:34:26 +0200

    ajuste

--- open_fstat_close.c
+++ open_fstat_close.c
@@ -1,15 +1,7 @@
 #include "all_system.h"
 #include <sys/time.h>
 
-
-
-#define KB  (1024)
-
-#define BUFF_SIZE (1*MB)
-char buffer[BUFF_SIZE] ; 
-
 #include <sys/stat.h>
-
 
 double get_time(void)
 {
@@ -22,8 +14,7 @@ double get_time(void)
 
 int main ( int argc, char *argv[] )
 {
-	int	ret,fd1;
-	double t_bc,t_ac,t_bw,t_aw;
+	int	ret,fd,result;
 	struct stat statbuf;
 
 	if (argc < 3)
@@ -37,43 +28,42 @@ int main ( int argc, char *argv[] )
 		return -1 ;
 	}
 
-	memset(buffer, 'a', BUFF_SIZE) ;
-	t_bc = get_time();
-	
-	fd1 = open(argv[1], O_RDWR);
-	if (fd1 < 0) {
+	result = stat(argv[1], &statbuf);
+	if (result == -1)
+	{
+		printf("Error getting file status\n");
+		return -1;
+	}
+	printf("%d = stat('%s', %p)\n", result, argv[1], &statbuf);
+
+	result = lstat(argv[1], &statbuf);
+	if (result == -1)
+	{
+		printf("Error getting file status\n");
+		return -1;
+	}
+	printf("%d = lstat('%s', %p)\n", result, argv[1], &statbuf);
+	printf("File size: %lld bytes\n", (long long)statbuf.st_size);
+	printf("File permissions: %o\n", statbuf.st_mode & 0777);
+
+	fd = open(argv[1], O_RDWR);
+	if (fd < 0) {
 		printf("Error opening file\n");
 		return -1 ;
 	}
-	printf("%d = open('%s', %o)\n", fd1, argv[1], 00777);
+	printf("%d = open('%s', %o)\n", fd, argv[1], 00777);
 
-	int result = fstat(fd1, &statbuf);
+	result = fstat(, &statbuf);
 	if(result == -1)
 	{
 		printf("Error getting file status\n");
 		return -1;
 	}
-	printf("%d = fstat(%d, %p)\n", result, fd1, &statbuf);
+	printf("%d = fstat(%d, %p)\n", result, fd, &statbuf);
 	printf("File size: %lld bytes\n", (long long)statbuf.st_size);
 	printf("File permissions: %o\n", statbuf.st_mode & 0777);
 
-	t_bw = get_time(); 
-	
-	long mb = atoi(argv[2]) ;
-	// for (int i = 0; i < mb; i++)
-	// {
-	// 	ret = read(fd1, buffer, BUFF_SIZE);
-	// 	printf("%d = read_%d(%d, %p, %lu)\n", ret, i, fd1, buffer, (unsigned long)BUFF_SIZE);
-	// }
-
-	t_aw = get_time() - t_bw;
-
 	ret = close(fd1);
 	printf("%d = close(%d)\n", ret, fd1) ;
-
-	t_ac = get_time() - t_bc;
-
-	printf("Bytes (KiB); Total time (ms); Read time (ms)\n") ;
-	printf("%f;%f;%f\n", ((double)mb * (double)BUFF_SIZE) / ((double)KB), t_ac * 1000, t_aw * 1000) ;
 	return 0;
 }
